9f20215aed5cd14395567d3abe2b52fb689cf6d8
Mauricio Vieira <mauricio@mauriciovieira.net>
2015-09-22 23:49:13 -0300

    Add hook methods

--- src/main/java/com/clicksign/models/Hook.java
+++ src/main/java/com/clicksign/models/Hook.java
@@ -1,5 +1,8 @@
 package com.clicksign.models;
 
+import java.util.Map;
+
+import com.clicksign.exception.ClicksignException;
 import com.clicksign.net.ClicksignResource;
 
 public class Hook extends ClicksignResource {
@@ -26,5 +29,22 @@ public class Hook extends ClicksignResource {
 
 	public void setUrl(String url) {
 		this.url = url;
+	}
+
+	public static HookCollection all(Document document, String accessToken) throws ClicksignException {
+		return request(RequestMethod.GET, instanceURL(Document.class, document.getKey(), Hook.class), null,
+				HookCollection.class, accessToken);
+	}
+
+	public static Hook create(Document document, String url, String accessToken) throws ClicksignException {
+		// TODO use Url as a parameter
+		Map<String, Object> params = null;
+		return request(RequestMethod.POST, instanceURL(Document.class, document.getKey(), Hook.class), params,
+				Hook.class, accessToken);
+	}
+
+	public static Hook delete(Document document, String url, String accessToken) throws ClicksignException {
+		return request(RequestMethod.DELETE, instanceURL(Document.class, document.getKey(), Hook.class), null,
+				Hook.class, accessToken);
 	}
 }
---
+++ src/main/java/com/clicksign/models/HookCollection.java
@@ -0,0 +1,15 @@
+package com.clicksign.models;
+
+import java.util.List;
+
+public class HookCollection {
+	List<Hook> hooks;
+
+	public List<Hook> getHooks() {
+		return hooks;
+	}
+
+	public void setHooks(List<Hook> hooks) {
+		this.hooks = hooks;
+	}
+}
